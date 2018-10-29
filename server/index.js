d62348fae7bb73478349cd4432fb8406ad15833e
Rakesh Joshi <rakeshjoshi9999@gmail.com>
2018-10-29 18:29:35 +0530

    Initail Commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+var publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var server = http.createServer(app);
+app.use(express.static(publicPath));
+const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+    console.log('New User Connected!');
+
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected!')
+    })
+});
+
+
+server.listen(3000, () => {
+    console.log('App running on port 3000')
+});
