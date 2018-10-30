b3b59b6285da5430ba3086783a305ac8912092b8
Rakesh Joshi <rakeshjoshi9999@gmail.com>
2018-10-30 05:55:48 +0530

    Changed Hardcoded Port to process.env.PORT

--- server/index.js
+++ server/index.js
@@ -30,6 +30,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3000, () => {
-    console.log('App running on port 3000')
+server.listen(port, () => {
+    console.log(`App Running at Port: ${port}`);
 });
