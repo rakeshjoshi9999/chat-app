fe41efc17edf543d564112bedab4a3fff1fe65e9
Rakesh Joshi <rakeshjoshi9999@gmail.com>
2018-10-31 13:08:16 +0530

    Initial Working Messenger

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,29 @@
 var socket = io();
-
+// listening for the connect event
 socket.on('connect', () => {
     console.log('Connected to the server');
 });
-
-
+// listening for the disconnect event 
 socket.on('disconnect', () => {
     console.log('Disconnected from server!')
 });
 
-socket.on('incomingMessage', (message) => {
-    console.log('Recieved a message', message)
+// listening for the new message from server
+socket.on('newMessage', (message) => {
+    // console.log('Recieved a message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}:${message.text}`);
+
+    jQuery('#messages').append(li);
 });
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    // emit a event when a new message is submitted from the form
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    });
+    jQuery('#message-form')[0].reset();
+});
+
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const { generateMessage } = require('./utils/message')
 
 var publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -14,16 +15,19 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('New User Connected!');
+    // emit message on Connecting to server
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
+    //broadcast a message to everyone on the network of new user joining
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
+
+    // listen to a event from client for creating a message and generate a message from form values
     socket.on('createMessage', (message) => {
-        console.log("new messgae at server", message)
-        io.emit('incomingMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        console.log('new messgae at server', message)
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
 
+    //listen to a event when user disconnects from server
     socket.on('disconnect', () => {
         console.log('User Disconnected!')
     });
